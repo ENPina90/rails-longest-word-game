c9c96521ab6081d4299351a628d45d60da21c5b4
Edward Pina <en.pina1@gmail.com>
2021-02-20 22:47:14 +0100

    template & added score

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,35 @@
+require "open-uri"
+require "date"
+
+class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
+  def new
+    @start = Time.now
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
+  end
+
+  def score
+    finish = Time.now
+    time = finish - params[:start].to_time
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+    @score = (@word.length * 10) / time.to_i
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
